5da4e0f1aa3e05dd21d48aaeadc50b4baf8005e7
Sharath <sharathwelcomesyouin@gmail.com>
2022-06-22 14:54:49 +0530

    add input, label, button for GUI

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-import tkinter
+# import tkinter
+from tkinter import *
 
-my_window=tkinter.Tk()
+my_window = Tk()
 my_window.title("Hello World")
-my_window.minsize(width=500,height=300)
+my_window.minsize(width=500, height=300)
 
-name_label=tkinter.Label(text="Name:", font=("Arial",12,"italic"))
+name_label = Label(text="Name:", font=("Arial", 12, "italic"))
 # name_label.pack(side="left")
 # name_label.pack(side="bottom")
 # name_label.pack(expand=True)
 name_label.pack()
 
+user_input=Entry()
+user_input.pack()
+
+
+def on_click_button():
+    # print("Wow, you cliked?!")
+    # name_label["text"]="Change the name"
+    # name_label.config(text="you clicked on Button")
+    user_value= user_input.get()
+    name_label.config(text=user_value)
+
+click_button = Button(text="Click Me", command=on_click_button)
+
+click_button.pack()
 my_window.mainloop()
